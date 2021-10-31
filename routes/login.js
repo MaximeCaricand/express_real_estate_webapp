324cc4f95bd2322186892d2d170a90de4ec99715
cm171529 <maxime.caricand@etu.unit-lehavre.fr>
2021-10-31 18:04:23 +0100

    login form validation

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,23 @@
-const lib = require('../lib/index');
 var express = require('express');
+const bcrypt = require('bcrypt');
+const { userService } = require('../lib/index');
 var router = express.Router();
 
 /* GET login listing. */
 router.get('/', function (req, res, next) {
-  res.render('pages/login');
+    res.render('pages/login');
 });
+
+router.post('/', async function (req, res, next) {
+    const user = await userService.getUser(req.body.email);
+    if (user) {
+        const passwordIsValid = await bcrypt.compare(req.body.password, user.hash);
+        if (passwordIsValid) {
+            return res.redirect('../')
+        }
+    }
+    return res.render('pages/login', { error: 'Invalid email or password' });
+});
+
 
 module.exports = router;
