b81e498f596355be7084cccdfcb931390794257c
hf171907 <florian.henry@etu.univ-lehavre.fr>
2021-11-05 04:51:35 +0100

    fix details et modification des commentaires

--- public/js/detail.js
+++ public/js/detail.js
@@ -1,10 +1,24 @@
+//This function is used to change the source of an image after an onclick
 function switchImage(newImage) {
     document.getElementById("myImageView").src = newImage;
 }
 
-function addQuestion() {
-    alert(document.getElementById('askQuestion').value);
+//This function is used to register a new question for an ad
+//idProperty : the id of the ad, for which the question is added
+function askQuestion(idProperty) {
+    alert(idProperty + " askQuestion : " + document.getElementById('askQuestion').value);
     //Maxime, faut ajouter le commentaire dans la base
+    
+
+    location = location;
+}
+
+//This function is used to record a new answer for a question in an ad
+//idProperty : the id of the ad, for which the answer is added
+//indQuestion: the index of the question, which is answered
+function ansQuestion(idProperty, indQuestion) {
+    alert(idProperty + " ansQuestion : " + document.getElementById('ansQuestion').value + " q°" + indQuestion);
+    //Maxime, faut ajouter la réponse dans la base
     
 
     location = location;
