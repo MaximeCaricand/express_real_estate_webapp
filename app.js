dd7f9dac9826dd3b2b967f0130680c1ae4042ca5
hf171907 <florian.henry@etu.univ-lehavre.fr>
2021-11-05 21:41:24 +0100

    ejs for errors pages 404 + 500

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ connect();
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+    res.render('errors/404');
 });
 
 // error handler
@@ -63,7 +63,7 @@ app.use(function (err, req, res, next) {
 
     // render the error page
     res.status(err.status || 500);
-    res.render('error');
+    res.render('errors/error');
 });
 
 app.locals.getVal = function (locals, fieldName) {
